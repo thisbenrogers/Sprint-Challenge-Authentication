839fc95d8292764679600a72df947099d4de959b
Ben <thisbenrogers@gmail.com>
2019-11-15 10:31:35 -0600

    :lock: Restricts jokes route

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,22 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+  const token = req.header.authorization;
+
+  if (token) {
+    const secret = process.env.JWT_SECRET || 'Let me tell you a myth about secrets...';
+
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
+
 };
