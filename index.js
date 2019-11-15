0e842bcbde0bce54a5ddf6e85e8fd80f4a581509
Ben <thisbenrogers@gmail.com>
2019-11-15 11:35:23 -0600

    :heavy_plus_sign: Adds testing dependencies

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const PORT = process.env.PORT || 3300;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
+
+
+// TODO document the auth process from scratch. Building auth should have taken more than one commit. 
