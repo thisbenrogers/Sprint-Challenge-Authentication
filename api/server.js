a901bc4817374cef610fa901402cccf8d23afbaa
Ben <thisbenrogers@gmail.com>
2019-11-15 10:27:21 -0600

    :sparkles: Runs server
    Adds logger middleware, gets to /

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const logger = require('../middleware/logger');
+
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
@@ -9,10 +11,15 @@ const jokesRouter = require('../jokes/jokes-router.js');
 const server = express();
 
 server.use(helmet());
+server.use(logger);
 server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+server.get('/', (req, res) => {
+  res.send('<h1>🚀</h1>');
+})
 
 module.exports = server;
