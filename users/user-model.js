af6a3f44e915df110b12c665c068febc9bd77b5a
Ben <thisbenrogers@gmail.com>
2019-11-15 12:56:23 -0600

    :white_check_mark: Adds tests
    Test fail and pass as expected

--- users/user-model.js
+++ users/user-model.js
@@ -8,7 +8,7 @@ module.exports = {
 };
 
 function find() {
-  return db('users').select('id', 'username', 'department');
+  return db('users').select('id', 'username');
 }
 
 function findBy(filter) {
